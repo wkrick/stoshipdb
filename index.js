9234f0409d6830604ed55a7b1f36c0014c03f721
William Krick <wkrick@gmail.com>
2021-04-02 20:36:36 -0400

    added more filters

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const app = Vue.createApp({
 	data() {
 		return {
-			faction: "All",
-      flanking: "All",
+			name: "",
+			source: "All",
+			fa: "All",
+			fore: "All",
+			aft: "All",
+			flanking: "All",
 			allShips: data
 		}
 	},
@@ -11,71 +15,67 @@ const app = Vue.createApp({
 			
 			var ships = this.allShips;
 
-			if (this.faction !== "All") {
-				ships = ships.filter(ship => ship.faction==this.faction);
+			if (this.name !== "") {
+				ships = ships.filter(ship => ship.name.toLowerCase().includes(this.name.toLowerCase()));
+			}
+
+			if (this.source !== "All") {
+				ships = ships.filter(ship => ship.source===this.source);
+			}
+
+			if (this.fore !== "All") {
+				ships = ships.filter(ship => ship.fore===this.fore);
+			}
+
+			if (this.aft !== "All") {
+				ships = ships.filter(ship => ship.aft===this.aft);
+			}
+
+
+			if (this.fa !== "All") {
+				ships = ships.filter(ship => ship.fa===this.fa);
 			}
 
 			
 			if (this.flanking !== "All") {
-				ships = ships.filter(ship => ship.flanking==this.flanking);
+				ships = ships.filter(ship => ship.flanking===this.flanking);
 			}
-			
 			
 			console.log("ships.length: " + ships.length)
 			
 			return ships;
 		},
-		factionOptions() {
-
-			var ships = this.allShips;
-			var key = "faction";
-			
-			var result = new Set();
-			ships.forEach(function(item) {
-        if (item.hasOwnProperty(key)) {
-            result.add(item[key]);
-        }
-    	});
-		
-			var opts = [{value: "All", text: "All"}];
-			
-			result.forEach(function(item) {
-				var obj = {};
-				obj.value = item;
-				obj.text = item;
-				opts.push(obj);
-			});
-			
-			console.log("factionOptions called");
-			
-			return opts;
+		sourceOptions() {
+			return this.getOpts("source");
+		},
+		foreOptions() {
+			return this.getOpts("fore");
+		},
+		aftOptions() {
+			return this.getOpts("aft");
+		},
+		faOptions() {
+			return this.getOpts("fa");
 		},
 		flankingOptions() {
-
-			var ships = this.allShips;
-			var key = "flanking";
+			return this.getOpts("flanking");
+		}
+	},
+	methods:{
+		getOpts(key) {
+			result = Array.from(new Set(this.allShips.map(item => item[key]))).sort();
+			result.unshift("All");
 			
-			var result = new Set();
-			ships.forEach(function(item) {
-        if (item.hasOwnProperty(key)) {
-            result.add(item[key]);
-        }
-    	});
-		
-			var opts = [{value: "All", text: "All"}];
-			
+			var opts = [];
 			result.forEach(function(item) {
 				var obj = {};
 				obj.value = item;
 				obj.text = item;
 				opts.push(obj);
 			});
-			
-			console.log("flankingOptions called");
 			
 			return opts;
 		}
-
 	}
 })
 
