7e932d0cb7a54709e179f1acd2103c4e7d77eb0f
William Krick <wkrick@gmail.com>
2021-04-03 00:43:54 -0400

    added more filters

--- index.js
+++ index.js
@@ -13,6 +13,20 @@ const app = Vue.createApp({
 			aft: "All",
 			dhc: "All",
 			exp: "All",
+			hangars: "All",
+			dev: "All",
+			fleet: "Yes",
+			taccons: "All",
+			engcons: "All",
+			scicons: "All",
+			unicons: "All",
+
+			secdef: "All",
+			subtargeting: "All",
+			sensoranalysis: "All",
+			singularity: "All",
+			cloak: "All",
+
 			flanking: "All",
 			allShips: data
 		}
@@ -70,6 +84,54 @@ const app = Vue.createApp({
 				ships = ships.filter(ship => ship.exp===this.exp);
 			}
 
+			if (this.hangars !== "All") {
+				ships = ships.filter(ship => ship.hangars===this.hangars);
+			}
+
+			if (this.dev !== "All") {
+				ships = ships.filter(ship => ship.dev===this.dev);
+			}
+
+			if (this.fleet !== "All") {
+				ships = ships.filter(ship => ship.fleet===this.fleet);
+			}
+
+			if (this.taccons !== "All") {
+				ships = ships.filter(ship => ship.taccons===this.taccons);
+			}
+
+			if (this.engcons !== "All") {
+				ships = ships.filter(ship => ship.engcons===this.engcons);
+			}
+
+			if (this.scicons !== "All") {
+				ships = ships.filter(ship => ship.scicons===this.scicons);
+			}
+
+			if (this.unicons !== "All") {
+				ships = ships.filter(ship => ship.unicons===this.unicons);
+			}
+			
+			if (this.secdef !== "All") {
+				ships = ships.filter(ship => ship.secdef===this.secdef);
+			}
+
+			if (this.subtargeting !== "All") {
+				ships = ships.filter(ship => ship.subtargeting===this.subtargeting);
+			}
+
+			if (this.sensoranalysis !== "All") {
+				ships = ships.filter(ship => ship.sensoranalysis===this.sensoranalysis);
+			}
+
+			if (this.singularity !== "All") {
+				ships = ships.filter(ship => ship.singularity===this.singularity);
+			}
+
+			if (this.cloak !== "All") {
+				ships = ships.filter(ship => ship.cloak===this.cloak);
+			}
+
 			if (this.flanking !== "All") {
 				ships = ships.filter(ship => ship.flanking===this.flanking);
 			}
@@ -112,6 +174,42 @@ const app = Vue.createApp({
 		},
 		expOptions() {
 			return this.getOpts("exp");
+		},
+		hangarsOptions() {
+			return this.getOpts("hangars");
+		},
+		devOptions() {
+			return this.getOpts("dev");
+		},
+		fleetOptions() {
+			return this.getOpts("fleet");
+		},
+		tacconsOptions() {
+			return this.getOpts("taccons");
+		},
+		engconsOptions() {
+			return this.getOpts("engcons");
+		},
+		sciconsOptions() {
+			return this.getOpts("scicons");
+		},
+		uniconsOptions() {
+			return this.getOpts("unicons");
+		},
+		secdefOptions() {
+			return this.getOpts("secdef");
+		},
+		subtargetingOptions() {
+			return this.getOpts("subtargeting");
+		},
+		sensoranalysisOptions() {
+			return this.getOpts("sensoranalysis");
+		},
+		singularityOptions() {
+			return this.getOpts("singularity");
+		},
+		cloakOptions() {
+			return this.getOpts("cloak");
 		},
 		flankingOptions() {
 			return this.getOpts("flanking");
