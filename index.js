49dde0940a438976c114e27c83784a1d2091f81f
William Krick <wkrick@gmail.com>
2022-02-18 22:30:48 -0500

    removed googleapis link and replaced with inline svg icons

--- index.js
+++ index.js
@@ -424,7 +424,13 @@ app.component('ability', {
 			<label class="input">{{ type }} - {{ name }} {{ level }} ({{ rank }})</label>
 		</div>
 		<div class="control">
-			<button @click="$emit('remove')" class="button is-info"><span class="material-icons">&#xe888;</span></button>
+			<button class="button is-info" @click="$emit('remove')">
+				<span class="icon is-small">
+					<svg style="width:24px;height:24px" viewBox="0 0 24 24">
+						<path fill="currentColor" d="M12,20C7.59,20 4,16.41 4,12C4,7.59 7.59,4 12,4C16.41,4 20,7.59 20,12C20,16.41 16.41,20 12,20M12,2C6.47,2 2,6.47 2,12C2,17.53 6.47,22 12,22C17.53,22 22,17.53 22,12C22,6.47 17.53,2 12,2M14.59,8L12,10.59L9.41,8L8,9.41L10.59,12L8,14.59L9.41,16L12,13.41L14.59,16L16,14.59L13.41,12L16,9.41L14.59,8Z" />
+					</svg>
+				</span>
+			</button>
 		</div>
 	</div>
 	`,
@@ -439,7 +445,13 @@ app.component('attribute', {
 			<label class="input">{{ name }} {{ comparison }} {{ value }}</label>
 		</div>
 		<div class="control">
-			<button @click="$emit('remove')" class="button is-info"><span class="material-icons">&#xe888;</span></button>
+			<button class="button is-info" @click="$emit('remove')">
+				<span class="icon is-small">
+					<svg style="width:24px;height:24px" viewBox="0 0 24 24">
+						<path fill="currentColor" d="M12,20C7.59,20 4,16.41 4,12C4,7.59 7.59,4 12,4C16.41,4 20,7.59 20,12C20,16.41 16.41,20 12,20M12,2C6.47,2 2,6.47 2,12C2,17.53 6.47,22 12,22C17.53,22 22,17.53 22,12C22,6.47 17.53,2 12,2M14.59,8L12,10.59L9.41,8L8,9.41L10.59,12L8,14.59L9.41,16L12,13.41L14.59,16L16,14.59L13.41,12L16,9.41L14.59,8Z" />
+					</svg>
+				</span>
+			</button>
 		</div>
 	</div>
 	`,
@@ -454,7 +466,13 @@ app.component('column', {
 			<label class="input">{{ name }}</label>
 		</div>
 		<div class="control">
-			<button @click="$emit('remove')" class="button is-info"><span class="material-icons">&#xe888;</span></button>
+			<button class="button is-info" @click="$emit('remove')">
+				<span class="icon is-small">
+					<svg style="width:24px;height:24px" viewBox="0 0 24 24">
+						<path fill="currentColor" d="M12,20C7.59,20 4,16.41 4,12C4,7.59 7.59,4 12,4C16.41,4 20,7.59 20,12C20,16.41 16.41,20 12,20M12,2C6.47,2 2,6.47 2,12C2,17.53 6.47,22 12,22C17.53,22 22,17.53 22,12C22,6.47 17.53,2 12,2M14.59,8L12,10.59L9.41,8L8,9.41L10.59,12L8,14.59L9.41,16L12,13.41L14.59,16L16,14.59L13.41,12L16,9.41L14.59,8Z" />
+					</svg>
+				</span>
+			</button>
 		</div>
 	</div>
 	`,
