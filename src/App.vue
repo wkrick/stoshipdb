e81aadc7db20c8801dfe3550dd80d92eb515ea97
William Krick <wkrick@gmail.com>
2022-04-06 23:26:02 -0400

    updated ship data

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,7 @@ import ShipSeatsInterface from './types/ShipSeats.interface'
 
 // data source: https://docs.google.com/spreadsheets/d/1-5Nmp_vycD2VpLbuqWnnQL1Lvx-nPy-cGYLPIckGRLk/edit?usp=sharing
 // created by Reddit user u/Fleffle
-// last updated 14 Feb 2022
+// last updated 06 Apr 2022
 import allShipsJSON from './assets/shipdata.json'
 import allSeatsJSON from './assets/seatdata.json'
 import allAttributesJSON from './assets/attributedata.json'
@@ -515,7 +515,7 @@ const testShip = (seats: SeatInterface[]) => {
 			@click="openURL('https://www.reddit.com/user/Fleffle')"
 		/>.</p>
 
-		<p>Last updated 2022-03-10</p>
+		<p>Last updated 2022-04-06</p>
 
 	<form>
 
