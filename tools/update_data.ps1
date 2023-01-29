00ee6c4ace66ed84accf6009f11f366b8f7d31f0
William Krick <wkrick@gmail.com>
2023-01-29 16:07:56 -0500

    added filter for total seats

--- tools/update_data.ps1
+++ tools/update_data.ps1
@@ -11,7 +11,7 @@ $client.DownloadFile($url,"$PSScriptRoot/$csvname")
 
 # write the csv data to a temp file, skipping the first two lines
 Get-ChildItem "$PSScriptRoot/$csvname" | ForEach-Object {
-    Set-Content (Get-Content $_ | Select-Object -Skip 2) -Path "$PSScriptRoot/temp"
+	Set-Content (Get-Content $_ | Select-Object -Skip 2) -Path "$PSScriptRoot/temp"
 }
 
 $header = @(
@@ -154,11 +154,11 @@ $header = @(
 # read in csv data from temp file and make some modifications while reading it
 $csvData = Import-Csv -Path "$PSScriptRoot/temp" -Header $header | ForEach-Object {
 
-    # remove abbreviations in name field
-    $_.nm = $_.nm.replace('MW', 'Miracle Worker')
-    $_.nm = $_.nm.replace('S31', 'Section 31');
+	# remove abbreviations in name field
+	$_.nm = $_.nm.replace('MW', 'Miracle Worker')
+	$_.nm = $_.nm.replace('S31', 'Section 31');
 
-    # add (T6) to some names as some URLs require it
+	# add (T6) to some names as some URLs require it
 	$_.nm = $_.nm.replace('Fleet Akira Heavy Strike Wing Escort', 'Fleet Akira Heavy Strike Wing Escort (T6)')
 	$_.nm = $_.nm.replace('Fleet Avenger Battlecruiser', 'Fleet Avenger Battlecruiser (T6)')
 	$_.nm = $_.nm.replace('Fleet Caitian Atrox Carrier', 'Fleet Caitian Atrox Carrier (T6)')
@@ -173,43 +173,43 @@ $csvData = Import-Csv -Path "$PSScriptRoot/temp" -Header $header | ForEach-Objec
 	$_.nm = $_.nm.replace('Risian Luxury Cruiser', 'Risian Luxury Cruiser (T6)')
 	$_.nm = $_.nm.replace('Risian Pilot corvette', 'Risian Pilot corvette (T6)')
 
-    # remove abbreviations from mastery package
-    $_.mp = $_.mp.replace('WB','Warbird')
-    $_.mp = $_.mp.replace('Dread ', 'Dreadnought ') # trailing space is needed
+	# remove abbreviations from mastery package
+	$_.mp = $_.mp.replace('WB','Warbird')
+	$_.mp = $_.mp.replace('Dread ', 'Dreadnought ') # trailing space is needed
 
-    # attempt to correct any missing URLs
-    if (-not ($_.url -like "https*")) {
-        $_.url = 'https://sto.fandom.com/wiki/' + [System.Web.HttpUtility]::UrlEncode($_.nm.replace(' ', '_'))
-    }
+	# attempt to correct any missing URLs
+	if (-not ($_.url -like "https*")) {
+		$_.url = 'https://sto.fandom.com/wiki/' + [System.Web.HttpUtility]::UrlEncode($_.nm.replace(' ', '_'))
+	}
 
-    # correct the Science Destroyers to actually be science mode
-    if ($_.nm -eq 'Titan Science Destroyer') {
-        $_.nm = $_.nm + ' (Science Mode)'
-        $_.exp = 'No'
-        $_.boff2rank = 3
-    }
-    if ($_.nm -eq 'Section 31 Intel Science Destroyer') {
-        $_.nm = $_.nm + ' (Science Mode)'
-        $_.exp = 'No'
-        $_.boff2rank = 3
-    }
-    if ($_.nm -eq 'Fleet Comet Science Destroyer') {
-        $_.nm = $_.nm + ' (Science Mode)'
-        $_.exp = 'No'
-        $_.boff2rank = 3
-    }
-    if ($_.nm -eq 'Courage Command Science Destroyer') {
-        $_.nm = $_.nm + ' (Science Mode)'
-        $_.exp = 'No'
-        $_.boff2rank = 3
-    }
-    if ($_.nm -eq 'Mirror Crossfield Science Destroyer') {
-        $_.nm = $_.nm + ' (Science Mode)'
-        $_.exp = 'No'
-        $_.boff2rank = 3
-    }
+	# correct the Science Destroyers to actually be science mode
+	if ($_.nm -eq 'Titan Science Destroyer') {
+		$_.nm = $_.nm + ' (Science Mode)'
+		$_.exp = 'No'
+		$_.boff2rank = 3
+	}
+	if ($_.nm -eq 'Section 31 Intel Science Destroyer') {
+		$_.nm = $_.nm + ' (Science Mode)'
+		$_.exp = 'No'
+		$_.boff2rank = 3
+	}
+	if ($_.nm -eq 'Fleet Comet Science Destroyer') {
+		$_.nm = $_.nm + ' (Science Mode)'
+		$_.exp = 'No'
+		$_.boff2rank = 3
+	}
+	if ($_.nm -eq 'Courage Command Science Destroyer') {
+		$_.nm = $_.nm + ' (Science Mode)'
+		$_.exp = 'No'
+		$_.boff2rank = 3
+	}
+	if ($_.nm -eq 'Mirror Crossfield Science Destroyer') {
+		$_.nm = $_.nm + ' (Science Mode)'
+		$_.exp = 'No'
+		$_.boff2rank = 3
+	}
 
-    $_
+	$_
 }
 
 # we can delete the csv and temp files now
@@ -220,63 +220,63 @@ Remove-Item -Path "$PSScriptRoot/temp"
 $list_science_destroyers = 'Titan Science Destroyer (Science Mode)', 'Section 31 Intel Science Destroyer (Science Mode)', 'Fleet Comet Science Destroyer (Science Mode)', 'Courage Command Science Destroyer (Science Mode)', 'Mirror Crossfield Science Destroyer (Science Mode)'
 Foreach ($sd in $list_science_destroyers) {
 
-    # get the object from the csv collection
-    $row = $csvData | Where-Object -Property nm -eq -Value $sd
+	# get the object from the csv collection
+	$row = $csvData | Where-Object -Property nm -eq -Value $sd
 
-    # make a deep copy using serialize/deserialize (this might be overkill, but it works)
-    $row = [System.Management.Automation.PSSerializer]::Deserialize( [System.Management.Automation.PSSerializer]::Serialize( $row ) )
+	# make a deep copy using serialize/deserialize (this might be overkill, but it works)
+	$row = [System.Management.Automation.PSSerializer]::Deserialize( [System.Management.Automation.PSSerializer]::Serialize( $row ) )
 
-    # modify the copy to have Tactical Mode stats
+	# modify the copy to have Tactical Mode stats
 
-    # change the name
-    $row.nm = $row.nm.Replace('Science Mode', 'Tactical Mode')
-    
-    # adjust the seating
-    $row.boff1rank = 3
-    $row.boff2rank = 4
+	# change the name
+	$row.nm = $row.nm.Replace('Science Mode', 'Tactical Mode')
 
-    # Enable the experimental weapon
-    $row.exp = 'Yes'
+	# adjust the seating
+	$row.boff1rank = 3
+	$row.boff2rank = 4
 
-    # set the "science stuff" to "No"
-    $row.sd = 'No'
-    $row.sa = 'No'
-    $row.st = 'No'
+	# Enable the experimental weapon
+	$row.exp = 'Yes'
 
-    # Note: dividing by 1 converts strings to numbers
+	# set the "science stuff" to "No"
+	$row.sd = 'No'
+	$row.sa = 'No'
+	$row.st = 'No'
 
-    if ($sd -eq 'Courage Command Science Destroyer (Science Mode)') {
+	# Note: dividing by 1 converts strings to numbers
 
-        # reduce the shield capacity by 20%
-        $row.smd = $row.smd/1 * 0.8
-		
-        # increase the hull capacity by 20%
-        $row.hmd = $row.hmd/1 * 1.2
+	if ($sd -eq 'Courage Command Science Destroyer (Science Mode)') {
 
-    } else {
-        
-        # reduce the shield capacity by 10%
-        $row.smd = $row.smd/1 * 0.9
+		# reduce the shield capacity by 20%
+		$row.smd = $row.smd/1 * 0.8
 
-        # increase the impulse by 10%
-        $row.imp = $row.imp/1 * 1.1
+		# increase the hull capacity by 20%
+		$row.hmd = $row.hmd/1 * 1.2
 
-        # increase the turn rate and inertia
-        $row.trn = $row.trn/1 + 2
-        $row.inr = $row.inr/1 + 10
+	} else {
 
-        # modify the bonus power
-        if ($sd -eq 'Mirror Crossfield Science Destroyer (Science Mode)') {
-            $row.ba = 0
-            $row.bw = 15
-        } else {
-            $row.ba = 5
-            $row.bw = 15
-        }
-    }
+		# reduce the shield capacity by 10%
+		$row.smd = $row.smd/1 * 0.9
 
-    # add it to the collection
-    $csvData += $row
+		# increase the impulse by 10%
+		$row.imp = $row.imp/1 * 1.1
+
+		# increase the turn rate and inertia
+		$row.trn = $row.trn/1 + 2
+		$row.inr = $row.inr/1 + 10
+
+		# modify the bonus power
+		if ($sd -eq 'Mirror Crossfield Science Destroyer (Science Mode)') {
+			$row.ba = 0
+			$row.bw = 15
+		} else {
+			$row.ba = 5
+			$row.bw = 15
+		}
+	}
+
+	# add it to the collection
+	$csvData += $row
 
 }
 
@@ -295,12 +295,12 @@ $list_null_to_no = "dhc", "exp", "flt", "ccw", "ccs", "cce", "cct", "sd", "st", 
 
 # helper function to convert seat type/spec into a numeric code
 function seatType {
-    $seatTypes = @{ Uni = 0; Tac = 1; Eng = 2; Sci = 3; Int = 4; Cmd = 5; Pil = 6; Tmp = 7; MW = 8 }
-    if ($args[0]) {
-        $seatTypes[$args[0]]
-    } else {
-        0
-    }
+	$seatTypes = @{ Uni = 0; Tac = 1; Eng = 2; Sci = 3; Int = 4; Cmd = 5; Pil = 6; Tmp = 7; MW = 8 }
+	if ($args[0]) {
+		$seatTypes[$args[0]]
+	} else {
+		0
+	}
 }
 
 # start of ship/seat JSON data
@@ -309,98 +309,102 @@ $seatdata = "["
 
 $csvData | Foreach-Object {
 
-    # reset these variables for each iteration
-    $tcn = 0
-    $tab = 0
-    $colseparator = ''
-    $seat = ''
-    $seatseparator = ''
+	# reset these variables for each iteration
+	$tcn = 0 # total consoles
+	$tab = 0 # total abilities
+	$ts = 0 # total seats
+	$colseparator = ''
+	$seat = ''
+	$seatseparator = ''
 
-    # build the row for each ship
-    $shipdata += "$rowseparator$return    {`"id`"`:$count,"
+	# build the row for each ship
+	$shipdata += "$rowseparator$return    {`"id`"`:$count,"
 
-    foreach ($property in $_.PSObject.Properties)
-    {
-        $name = $property.Name
-        $value = $property.Value
+	foreach ($property in $_.PSObject.Properties) {
 
-        if (-not (($name -like 'skip*') -or ($name -like 'boff*'))) {
+		$name = $property.Name
+		$value = $property.Value
 
-            if ($value -match "^[+-]?([0-9]*[.])?[0-9]+$") {
-                $shipdata += "$colseparator`"$name`"`:$value"
-            } elseif ($value -eq "") {
-                if ($list_null_to_none -contains $name) {
-                    $shipdata += "$colseparator`"$name`"`:`"(None)`""
-                } elseif ($list_null_to_zero -contains $name) {
-                    $shipdata += "$colseparator`"$name`"`:0"
-                } elseif ($list_null_to_no -contains $name) {
-                    $shipdata += "$colseparator`"$name`"`:`"No`""
-                } else {
-                    # found something we didn't expect
-                    "unexpected null value in: $name"
-                }
-            } else {
-                $shipdata += "$colseparator`"$name`"`:`"$value`""
-            }
-            $colseparator = ','
+		if (-not (($name -like 'skip*') -or ($name -like 'boff*'))) {
 
-            # if this property is a console count, add it to the total
-            if (@("ct", "ce", "cs", "cu") -contains $name) {
-                $tcn += $value
-            }
+			if ($value -match "^[+-]?([0-9]*[.])?[0-9]+$") {
+				$shipdata += "$colseparator`"$name`"`:$value"
+			} elseif ($value -eq "") {
+				if ($list_null_to_none -contains $name) {
+					$shipdata += "$colseparator`"$name`"`:`"(None)`""
+				} elseif ($list_null_to_zero -contains $name) {
+					$shipdata += "$colseparator`"$name`"`:0"
+				} elseif ($list_null_to_no -contains $name) {
+					$shipdata += "$colseparator`"$name`"`:`"No`""
+				} else {
+					# found something we didn't expect
+					"unexpected null value in: $name"
+				}
+			} else {
+				$shipdata += "$colseparator`"$name`"`:`"$value`""
+			}
+			$colseparator = ','
 
-            # insert the total consoles after universal consoles
-            if ($name -eq "cu") {
-                $shipdata += "$colseparator`"tcn`"`:$tcn"
-            }
+			# if this property is a console count, add it to the total
+			if (@("ct", "ce", "cs", "cu") -contains $name) {
+				$tcn += $value
+			}
 
-        }
+			# insert the total consoles after universal consoles
+			if ($name -eq "cu") {
+				$shipdata += "$colseparator`"tcn`"`:$tcn"
+			}
 
-        if ($name -like 'boff*rank') {
-            $tab += $value
+		}
 
-            if ($value) {
-                $seat += "$seatseparator$value"
-                $seatseparator = ', '
-            }
-        }
-        if ($name -like 'boff*type') {
-            if ($value) {
-                $seat += " $value"
-            }
-        }
-        if ($name -like 'boff*spec') {
-            if ($value) {
-                $seat += "/$value"
-            }
-        }
-        
-    }
-    # append total abilities to end of data
-    $shipdata += "$colseparator`"tab`"`:$tab"
+		if ($name -like 'boff*rank') {
+			$tab += $value
 
-    # append a string representation of the seating
-    $shipdata += "$colseparator`"seat`"`:`"$seat`""
+			if ($value) {
+				$ts++
+				$seat += "$seatseparator$value"
+				$seatseparator = ', '
+			}
+		}
+		if ($name -like 'boff*type') {
+			if ($value) {
+				$seat += " $value"
+			}
+		}
+		if ($name -like 'boff*spec') {
+			if ($value) {
+				$seat += "/$value"
+			}
+		}
 
-    $shipdata += '}'
+	}
+	# append total abilities to end of data
+	$shipdata += "$colseparator`"tab`"`:$tab"
 
-    # dump seat data
-    $seatdata += "$rowseparator$return    {`"id`"`:$count,`"seats`"`:["
-    $seatdata += "{`"rank`"`:$($_.boff1rank),`"type`"`:$(seatType $_.boff1type),`"spec`"`:$(seatType $_.boff1spec)}"
-    $seatdata += ",{`"rank`"`:$($_.boff2rank),`"type`"`:$(seatType $_.boff2type),`"spec`"`:$(seatType $_.boff2spec)}"
-    $seatdata += ",{`"rank`"`:$($_.boff3rank),`"type`"`:$(seatType $_.boff3type),`"spec`"`:$(seatType $_.boff3spec)}"
-    $seatdata += ",{`"rank`"`:$($_.boff4rank),`"type`"`:$(seatType $_.boff4type),`"spec`"`:$(seatType $_.boff4spec)}"
-    if ($_.boff5rank) {
-        $seatdata += ",{`"rank`"`:$($_.boff5rank),`"type`"`:$(seatType $_.boff5type),`"spec`"`:$(seatType $_.boff5spec)}"
-    }
-    if ($_.boff6rank) {
-        $seatdata += ",{`"rank`"`:$($_.boff6rank),`"type`"`:$(seatType $_.boff6type),`"spec`"`:$(seatType $_.boff6spec)}"
-    }
-    $seatdata += "]}"
+	# append total seats to end of data
+	$shipdata += "$colseparator`"ts`"`:$ts"
 
+	# append a string representation of the seating
+	$shipdata += "$colseparator`"seat`"`:`"$seat`""
 
-    $rowseparator = ','
-    $count++
+	$shipdata += '}'
+
+	# dump seat data
+	$seatdata += "$rowseparator$return    {`"id`"`:$count,`"seats`"`:["
+	$seatdata += "{`"rank`"`:$($_.boff1rank),`"type`"`:$(seatType $_.boff1type),`"spec`"`:$(seatType $_.boff1spec)}"
+	$seatdata += ",{`"rank`"`:$($_.boff2rank),`"type`"`:$(seatType $_.boff2type),`"spec`"`:$(seatType $_.boff2spec)}"
+	$seatdata += ",{`"rank`"`:$($_.boff3rank),`"type`"`:$(seatType $_.boff3type),`"spec`"`:$(seatType $_.boff3spec)}"
+	$seatdata += ",{`"rank`"`:$($_.boff4rank),`"type`"`:$(seatType $_.boff4type),`"spec`"`:$(seatType $_.boff4spec)}"
+	if ($_.boff5rank) {
+		$seatdata += ",{`"rank`"`:$($_.boff5rank),`"type`"`:$(seatType $_.boff5type),`"spec`"`:$(seatType $_.boff5spec)}"
+	}
+	if ($_.boff6rank) {
+		$seatdata += ",{`"rank`"`:$($_.boff6rank),`"type`"`:$(seatType $_.boff6type),`"spec`"`:$(seatType $_.boff6spec)}"
+	}
+	$seatdata += "]}"
+
+	$rowseparator = ','
+	$count++
 }
 
 # end of ship/seat JSON data
