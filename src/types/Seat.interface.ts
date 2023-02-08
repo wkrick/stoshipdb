9c20e5335b6dc450e76933abc8c74896a4f5839e
William Krick <wkrick@gmail.com>
2023-02-07 19:46:41 -0500

    remove unused files

--- src/types/Seat.interface.ts
+++
@@ -1,9 +0,0 @@
-import AbilityType from "./AbilityType.enum"
-
-interface SeatInterface {
-    rank: number,
-    type: AbilityType,
-    spec: AbilityType
-}
-
-export default SeatInterface
--- src/types/ShipSeats.interface.ts
+++
@@ -1,8 +0,0 @@
-import SeatInterface from "./Seat.interface";
-
-interface ShipSeatsInterface {
-    id: number,
-    seats: SeatInterface[]
-}
-
-export default ShipSeatsInterface
